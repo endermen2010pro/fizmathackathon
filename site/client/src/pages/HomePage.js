ee3b2597748ab2a3ee8cfe8bcfb0f305a34961bb
virg1n <qtilinq@gmail.com>
2022-12-17 22:28:42 +0600

    HeaderHomePage

--- site/client/src/pages/HomePage.js
+++ site/client/src/pages/HomePage.js
@@ -1,12 +1,32 @@
 import { Link } from 'react-router-dom'
+import line from '../img/line.png'
 
 const HomePage = () => {
     return (
         <div>
-            <div className='companyName'>
-                <h1> lgnis </h1>
+            <div className='firstBlock'>
+                <div className='header'>
+                    <h1> lgnis </h1>
+                    <img src={line} alt="" className='headerLine'/>
+                    <p className='paragraphHeader'>Description and other things. I don’t know what should be there. Just some text without any meaning.</p>
+                </div>
+            </div>
+            <div className='SecondBlock'>
+                <div className='info'>
+                    <h1>First</h1>
+                    <h1>Second</h1>
+                    <h1>Third</h1>
+                </div>
+                <h1 style={{"textAlign":"center", "color":"#D9D9D9"}}>Opportunities</h1>
+                <div className='navBlock'>
+                    
+                    <h1>Map</h1>
+                    <h1>Map</h1>
+                    <h1>Map</h1>
+                </div>
             </div>
         </div>
+        
     )
 }
 
