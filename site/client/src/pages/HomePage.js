6ed5bb07555f13985da27b27824f266cddc9bdf5
virg1n <qtilinq@gmail.com>
2022-12-18 00:10:03 +0600

    pattern of frontend

--- site/client/src/pages/HomePage.js
+++ site/client/src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import line from '../img/line.png'
+import sun from '../img/sun.png'
+import rect from '../img/rect.png'
 
 const HomePage = () => {
     return (
@@ -13,17 +15,58 @@ const HomePage = () => {
             </div>
             <div className='SecondBlock'>
                 <div className='info'>
-                    <h1>First</h1>
-                    <h1>Second</h1>
-                    <h1>Third</h1>
+                    <div className='miniInfo'>
+                        <img src={sun} alt="" className='sun'/>
+                        <div>
+                            <h1>First</h1>
+                            <p>3400 +</p>
+                        </div>
+                    </div>
+                    <div className='miniInfo'>
+                        <img src={sun} alt="" className='sun'/>
+                        <div>
+                            <h1>Second</h1>
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
+                    <div className='miniInfo'>
+                        <img src={sun} alt="" className='sun'/>
+                        <div>
+                            <h1>Hello</h1>
+                            <p>projects</p>
+                        </div>
+                    </div>
+                    <div className='miniInfo'>
+                        <img src={sun} alt="" className='sun'/>
+                        <div>
+                            <h1>Second</h1>
+                            <p>Hello World!</p>
+                        </div>
+                    </div>
                 </div>
-                <h1 style={{"textAlign":"center", "color":"#D9D9D9"}}>Opportunities</h1>
-                <div className='navBlock'>
-                    
-                    <h1>Map</h1>
-                    <h1>Map</h1>
-                    <h1>Map</h1>
+                <div className='cards'>
+                    <h1 style={{"textAlign":"center", "color":"#D9D9D9", "marginBottom":"150px"}}>Opportunities</h1>
+                    <div className='navBlock'>
+                        <div className='card'>
+                            <img src={rect} alt="" className='cardImg'/>
+                            <h1 className='cardTitle'>Map</h1>
+                        </div>
+                        <div className='card'>
+                            <img src={rect} alt="" className='cardImg'/>
+                            <h1 className='cardTitle'>Map</h1>
+                        </div>
+                        <div className='card'>
+                            <img src={rect} alt="" className='cardImg'/>
+                            <h1 className='cardTitle'>Map</h1>
+                        </div>
+                        <div className='card'>
+                            <img src={rect} alt="" className='cardImg'/>
+                            <h1 className='cardTitle'>Map</h1>
+                        </div>
+                    </div>
                 </div>
+                <h1>sadasd</h1>
+                
             </div>
         </div>
         
