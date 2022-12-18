f88fcb05d005c31c91f92b2aba8b37f2c769b833
virg1n <qtilinq@gmail.com>
2022-12-18 11:55:46 +0600

    HomePage

--- site/client/src/pages/HomePage.js
+++ site/client/src/pages/HomePage.js
@@ -5,8 +5,59 @@ import rect from '../img/rect.png'
 import { Miniinfo } from '../components/Miniinfo'
 import { Card } from 'react-bootstrap'
 import { MyCard } from '../components/MyCard'
+import React from 'react'
 
 const HomePage = () => {
+    class CardHeader extends React.Component {
+        render() {
+          const { image } = this.props;
+          var style = { 
+              backgroundImage: 'url(' + image + ')',
+          };
+          return (
+            <header id={image} className="card-header">
+                <img src={image} alt="sad" className='cardImage'/>
+              {/* <h4 className="card-header--title">News</h4> */}
+            </header>
+          )
+        }
+      }
+      
+    //   class Button extends React.Component {
+    //     render() {
+    //       return (
+    //         <button className="button button-primary">
+    //           <i className="fa fa-chevron-right"></i> Find out more
+    //         </button>
+    //       )
+    //     }
+    //   }
+      
+      class CardBody extends React.Component {
+        render() {
+          return (
+            <div className="card-body">
+              
+              <h2>{this.props.title}</h2>
+              
+              <p className="body-content">{this.props.text}</p>
+              
+              {/* <Button /> */}
+            </div>
+          )
+        }
+      }
+      
+      class Card extends React.Component {
+        render() {
+          return (
+            <article className="card">
+              <CardHeader image={'https://source.unsplash.com/user/erondu/600x400'}/>
+              <CardBody title={'Map'} text={'На данной карте вы можеет увидеть предсказуемые пожары'}/>
+            </article>
+          )
+        }
+      }
     return (
         <div>
             <div className='firstBlock'>
@@ -18,57 +69,35 @@ const HomePage = () => {
             </div>
             <div className='SecondBlock'>
                 <div className='info'>
-                    {/* <div className='miniInfo'>
-                        <img src={sun} alt="" className='sun'/>
-                        <div>
-                            <h1>First</h1>
-                            <p>3400 +</p>
-                        </div>
-                    </div>
-                    <div className='miniInfo'>
-                        <img src={sun} alt="" className='sun'/>
-                        <div>
-                            <h1>Second</h1>
-                            <p>Hello World!</p>
-                        </div>
-                    </div>
-                    <div className='miniInfo'>
-                        <img src={sun} alt="" className='sun'/>
-                        <div>
-                            <h1>Hello</h1>
-                            <p>projects</p>
-                        </div>
-                    </div> */}
-                    <Miniinfo title="First" body="Projects" color="#FF6853" />
-                    <Miniinfo title="First" body="Projects" color="#80D2C4" />
-                    <Miniinfo title="First" body="Projects" color="#80D2C4" />
-                    <Miniinfo title="First" body="Projects" color="#80D2C4" />
+        
+                    <Miniinfo title="3400" body="Projects" color="#FF6853" />
+                    <Miniinfo title="240+" body="людей спасено" color="#FF537C" />
+                    <Miniinfo title="240+" body="пожаров потушено" color="#80D2C4" />
+                    <Miniinfo title="240+" body="спасено деревеьев" color="#3F50E7" />
+                    <Miniinfo title="240+" body="пожаров обнуаружено" color="#95CB7C" />
                 </div>
                 <div className='cards'>
-                    <h1 style={{"textAlign":"center", "color":"#D9D9D9", "marginBottom":"150px"}}>Opportunities</h1>
+                    <h1 style={{"textAlign":"center", "color":"#D9D9D9", "marginBottom":"100px", "marginTop":"150px"}}>Opportunities</h1>
                     <div className='navBlock'>
-                        {/* <div className='card'>
-                            <img src={rect} alt="" className='cardImg'/>
-                            <h1 className='cardTitle'>Map</h1>
-                        </div>
-                        <div className='card'>
-                            <img src={rect} alt="" className='cardImg'/>
-                            <h1 className='cardTitle'>Map</h1>
-                        </div>
-                        <div className='card'>
-                            <img src={rect} alt="" className='cardImg'/>
-                            <h1 className='cardTitle'>Map</h1>
-                        </div>
-                        <div className='card'>
-                            <img src={rect} alt="" className='cardImg'/>
-                            <h1 className='cardTitle'>Map</h1>
-                        </div> */}
+                        {/* <MyCard title="Map"/>
                         <MyCard title="Map"/>
                         <MyCard title="Map"/>
-                        <MyCard title="Map"/>
-                        <MyCard title="Map"/>
+                        <MyCard title="Map"/> */}
+                        <article className="card">
+                            <CardHeader image={'https://source.unsplash.com/user/erondu/600x400'}/>
+                            <CardBody title={'Map'} text={'На данной карте вы можеет увидеть предсказуемые пожары'}/>
+                        </article>
+                        <article className="card">
+                            <CardHeader image={'https://source.unsplash.com/user/erondu/600x400'}/>
+                            <CardBody title={'Map'} text={'На данной карте вы можеет увидеть предсказуемые пожары'}/>
+                        </article>
+                        <article className="card">
+                            <CardHeader image={'https://source.unsplash.com/user/erondu/600x400'}/>
+                            <CardBody title={'Map'} text={'На данной карте вы можеет увидеть предсказуемые пожары'}/>
+                        </article>
                     </div>
                 </div>
+                
                 <h1>sadasd</h1>
                 
             </div>
